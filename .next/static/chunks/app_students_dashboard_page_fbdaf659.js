cfb058abe9188679c281a2c9dee07b8db4636202
lorddddiz <lordmalord19@gmail.com>
2025-05-19 11:05:38 +0800

    Delete .next directory

--- .next/static/chunks/app_students_dashboard_page_fbdaf659.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_3b5af4d2._.js",
-  "static/chunks/app_students_dashboard_page_7fd7813c.js"
-],
-    source: "dynamic"
-});
